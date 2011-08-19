f6fc68a0e59f0ecd853e7a18ab66c0ee51c11d07
John Hampton <pacopablo@pacopablo.com>
2011-08-19 01:28:43 -0700

    Fix syntax errors

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-VERSION = "0.1.12"
+VERSION = "0.1.13"
 
 data_files=[
     ('share/docs/elderberrypy', glob.glob('COPYING*')),
