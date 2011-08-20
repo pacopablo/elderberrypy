8adaf2b24cc79a597db01ed3b4f2c6b7af2e97e4
John Hampton <pacopablo@pacopablo.com>
2011-08-20 16:56:08 -0700

    - Added tests for ``RestrictedPath`` in ``remove_path``

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -8,17 +8,23 @@
 #
 # Author: John Hampton <pacopablo@pacopablo.com>
 
-from elderberrypy.fs import remove_path, count_files
+import elderberrypy.fs
 from elderberrypy.errors import RestrictedPath
 
 import os
 import random
 import string
 import time
+import logging
+log = logging.getLogger('elderberrypy.fs')
 
-from nose.tools import with_setup, assert_false
+from nose.tools import with_setup, assert_false, raises
 
 BASE_PATH = ''
+
+remove_path = elderberrypy.fs.remove_path
+count_files = elderberrypy.fs.count_files
+RESTRICTED_PATHS = elderberrypy.fs.RESTRICTED_PATHS
 
 def setup_files():
     """ Setup a directory tree with files to test ``count_files`` and
@@ -67,15 +73,57 @@ def teardown_files():
             pass
 
 
+def mock_remove():
+    """ Mock out ``os.remove`` so that we don't accidentally kill ourselves """
+    global SAVED_OS_REMOVE
+
+    def mock_os_remove(f):
+        log.debug('Restricted Path Test -- ERROR -- tired to delete: %s' % str(f))
+    SAVED_OS_REMOVE = elderberrypy.fs.os.remove
+    elderberrypy.fs.os.remove = mock_os_remove
+
+
+def restore_remove():
+    """ Put back ``os.remove`` incase needed by other tests. """
+    global SAVED_OS_REMOVE
+
+    elderberrypy.fs.os.remove = SAVED_OS_REMOVE
+
+
 @with_setup(setup_files, teardown_files)
 def test_count_files():
     global BASE_PATH
     assert(count_files(BASE_PATH) == 9)
 
+
+@with_setup(setup_files, teardown_files)
 def test_remove_path():
     global BASE_PATH
 
     for num_files in remove_path(BASE_PATH):
         continue
     assert_false(os.path.exists(BASE_PATH))
+
+
+@with_setup(mock_remove, restore_remove)
+def test_restricted_paths():
+    def remove_flush(path):
+        try:
+            for n in remove_path(path):
+                continue
+            raise AssertionError('RestrictedPath')
+        except RestrictedPath:
+            # We expect this
+            pass
+
+    for p in RESTRICTED_PATHS['explicit']:
+        yield remove_flush, p
+    for p in RESTRICTED_PATHS['tree']:
+        yield remove_flush, os.path.join(p, 'foobar')
+    try:
+        # A non-existant directory should not raise an error.
+        for n in remove_path('/lkasdkjfjkdsalkf'):
+            continue
+    except RestrictedPath:
+        raise
 
