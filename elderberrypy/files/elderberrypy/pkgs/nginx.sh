5f1f68c637276d274f7ea4e475db53b9211aa77f
John Hampton <pacopablo@pacopablo.com>
2011-12-11 04:36:44 +0300

    Fix nginx install via bundling pcre and add redhat startup script

--- elderberrypy/files/elderberrypy/pkgs/nginx.sh
+++ elderberrypy/files/elderberrypy/pkgs/nginx.sh
@@ -11,6 +11,9 @@
 # nginx info
 NGINX_VER=1.0.10
 NGINX_URL=http://nginx.org/download/nginx-${NGINX_VER}.tar.gz
+# libpcre info
+LIBPCRE_VER=8.20
+LIBPCRE_URL=http://downloads.sourceforge.net/project/pcre/pcre/${LIBPCRE_VER}/pcre-${LIBPCRE_VER}.tar.bz2
 
 function pkg_install(){
     # Download
@@ -19,8 +22,19 @@ function pkg_install(){
     if [ $? -ne 0 ]; then
         return $RVAL
     fi
+    wget ${LIBPCRE_URL} >> ${LOGFILE} 2>&1
+    RVAL=$?;
+    if [ $? -ne 0 ]; then
+        return $RVAL
+    fi
 
     # Unpack
+    tar xf pcre-${LIBPCRE_VER}.tar.bz2 >> ${LOGFILE} 2>&1
+    RVAL=$?;
+    if [ $? -ne 0 ]; then
+        return $RVAL
+    fi
+    PCRE_SRC="`pwd`/pcre-${LIBPCRE_VER}"
     tar xf nginx-${NGINX_VER}.tar.gz >> ${LOGFILE} 2>&1
     RVAL=$?;
     if [ $? -ne 0 ]; then
@@ -36,6 +50,7 @@ function pkg_install(){
 		--lock-path=${EBPY_SRV_ROOT}/opt/var/run/nginx/nginx.lock \
 		--sbin-path=${EBPY_SRV_ROOT}/opt/bin/nginx \
 		--pid-path=${EBPY_SRV_ROOT}/opt/var/run/nginx/nginx.pid \
+		--with-pcre=${PCRE_SRC} \
 		--with-http_ssl_module \
 		--with-http_realip_module \
 		--with-http_gzip_static_module \
